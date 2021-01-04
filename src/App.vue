60c41f72d1b6026b606f43d0b3d4644ee0ad4ac4
adi delibasic <adi.delibasic@gmail.com>
2021-01-04 04:39:29 +0100

    basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,53 @@
 <template>
-  <div>
-    Home
-    <router-view />
+  <div id="app">
+    <header>
+      <router-link to="/">
+        <h1>Vue <span>Movies</span> DB</h1>
+      </router-link>
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer></footer>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
+  &::selection {
+    background: transparentize(#fff1e6, $amount: 0.5);
+  }
+}
+
+body {
+  background: #264653;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 20px;
+  background-color: #2a9d8f;
+  box-shadow: 0px 0px 5px rgba(#000000, 0.2);
+  text-transform: uppercase;
+
+  h1 {
+    color: #f4a261;
+    font-size: 30px;
+
+    span {
+      color: #e9c46a;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,44 @@
 <template>
   <div class="home">
-    Home
+    <!-- Featured movie card -->
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img
+          src="https://images.pexels.com/photos/4553622/pexels-photo-4553622.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt="Featured movie poster"
+          class="featured-img"
+        />
+        <div class="detail">
+          <h3>Featured Movie</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel repellendus fugiat deserunt
+            perferendis, dignissimos reiciendis tempore autem fugit modi rem natus harum ipsa qui
+            delectus culpa voluptatibus mollitia dolorum molestiae.
+          </p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="scss">
+.home {
+  .featured-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 250px;
+      object-fit: cover;
+      object-position: center;
+      position: relative;
+      z-index: 0;
+    }
+  }
+}
+</style>
