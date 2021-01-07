eda6e3135a4e31f78645df3965f214a787783d61
adi delibasic <adi.delibasic@gmail.com>
2021-01-07 21:57:19 +0100

    router link

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,16 +1,18 @@
 <template>
-  <img
-    src="https://m.media-amazon.com/images/M/MV5BZmY5ZDMxODEtNWIwOS00NjdkLTkyMjktNWRjMDhmYjJjN2RmXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-    alt="Featured movie poster"
-    class="featured-img"
-  />
-  <div class="detail">
-    <h3>The Wire</h3>
-    <p>
-      Set in Baltimore, this show centers around the city's inner-city drug scene. It starts as
-      mid-level drug dealer, D'Angelo Barksdale beats a murder rap.
-    </p>
-  </div>
+  <router-link to="/movie/tt0306414">
+    <img
+      src="https://m.media-amazon.com/images/M/MV5BZmY5ZDMxODEtNWIwOS00NjdkLTkyMjktNWRjMDhmYjJjN2RmXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+      alt="Featured movie poster"
+      class="featured-img"
+    />
+    <div class="detail">
+      <h3>The Wire</h3>
+      <p>
+        Set in Baltimore, this show centers around the city's inner-city drug scene. It starts as
+        mid-level drug dealer, D'Angelo Barksdale beats a murder rap.
+      </p>
+    </div>
+  </router-link>
 </template>
 
 <script>
