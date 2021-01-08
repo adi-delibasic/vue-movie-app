ec1b9766a16b5b862751a97016a4f86564fb7a28
adi delibasic <adi.delibasic@gmail.com>
2021-01-08 20:19:31 +0100

    search module

--- src/store/index.js
+++ src/store/index.js
@@ -5,27 +5,6 @@ import search from './modules/search';
 
 export default createStore({
 
-  // state: {
-  //   keywords: '',
-  //   movies: [],
-  // },
-
-  // mutations: {
-  //   createMovieList(state, payload) {
-  //     state.movies = payload.value;
-  //   }
-  // },
-
-  // actions: {
-
-  // },
-
-  // getters: {
-  //   getMovies(state) {
-  //     return state.movies
-  //   }
-  // },
-
   modules: {
     search,
   },
