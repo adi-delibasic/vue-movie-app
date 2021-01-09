6b20ba83baf878b29328c59bbd870071519360b6
adi delibasic <adi.delibasic@gmail.com>
2021-01-09 20:45:20 +0100

    carousel slide component

--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="carousel" v-for="slide in slides" :key="slide.id">
-    <div class="carousel-movie">
-      <router-link :to="'/movie/' + slide.id">
-        <img :src="slide.poster" alt="Featured movie poster" class="featured-img" />
-        <div class="detail">
-          <h3>{{ slide.title }}</h3>
-          <p>
-            {{ slide.excerpt }}
-          </p>
-        </div>
-      </router-link>
-    </div>
+    <SingleSlide />
   </div>
 </template>
 
 <script>
 import { onBeforeMount, computed } from 'vue';
 import { useStore } from 'vuex';
+import SingleSlide from './SingleSlide.vue';
 export default {
+  components: {
+    SingleSlide
+  },
   setup() {
     const store = useStore();
     const currentSlide = computed(() => {
@@ -40,36 +34,5 @@ export default {
 <style lang="scss">
 .carousel {
   position: relative;
-  &-movie {
-    position: absolute;
-    width: 100%;
-  }
-  .featured-img {
-    display: block;
-    width: 100%;
-    height: 400px;
-    object-fit: cover;
-    object-position: center;
-    position: relative;
-    z-index: 0;
-  }
-  .detail {
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba($color: #000000, $alpha: 0.6);
-    z-index: 1;
-    padding: 15px;
-
-    h3 {
-      color: #fff;
-      margin-bottom: 15px;
-    }
-
-    p {
-      color: #fff;
-    }
-  }
 }
 </style>
---
+++ src/components/carousel/SingleSlide.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="carousel-movie">
+    <!-- <router-link :to="'/movie/' + slide.id">
+      <img :src="slide.poster" alt="Featured movie poster" class="featured-img" />
+      <div class="detail">
+        <h3>{{ slide.title }}</h3>
+        <p>{{ slide.excerpt }}</p>
+      </div>
+    </router-link> -->
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+
+export default {
+  setup() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.carousel-movie {
+  position: absolute;
+  width: 100%;
+}
+.featured-img {
+  display: block;
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+  object-position: center;
+  position: relative;
+  z-index: 0;
+}
+.detail {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba($color: #000000, $alpha: 0.6);
+  z-index: 1;
+  padding: 15px;
+
+  h3 {
+    color: #fff;
+    margin-bottom: 15px;
+  }
+
+  p {
+    color: #fff;
+  }
+}
+</style>
